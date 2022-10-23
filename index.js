d2e4382ca1231c4fe763e9fb965d0a4dacbf36de
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-10-23 10:55:12 -0400

    commit tree 8a662303fd171461717234ff8c3ab9fc3ae0706c

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const splitWord = word.split('')
+  const reverseWord = splitWord.reverse()
+  const joinReverse = reverseWord.join('')
+  if(word === joinReverse){
+    return true
+  }else{
+    return false
+  }
 }
+
 
 /* 
   Add your pseudocode here
+  if array of char of strong [ w o r d] === array backwards [ d r o w]
+  return true 
+  else false
 */
 
 /*
   Add written explanation of your solution here
+  if the fist letter equals the last and then there's on in the middle that has no
+  other match nor option => palindrome
+
+  OR
+
+  if the word displayed in ascendent order equals the word displayed backwards
 */
 
 // You can run `node index.js` to view these console logs
